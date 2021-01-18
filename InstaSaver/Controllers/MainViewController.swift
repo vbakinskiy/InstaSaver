70dabc2116c430039317366e813612ffbbd20d3f
Vyacheslav Bakinskiy <vyacheslav.bakinskiy@gmail.com>
2021-01-18 13:57:52 +0300

    Connected PreviewVC to MainVC (opening logic)

--- InstaSaver/Controllers/MainViewController.swift
+++ InstaSaver/Controllers/MainViewController.swift
@@ -56,12 +56,55 @@ class MainViewController: UIViewController {
         interstitialAd = AdManager.createAndLoadInterstitial(vc: self)
     }
     
+    private func showPreview(_ link: String) {
+        guard let activeLink = InstaManager.checkTheLink(link) else {
+            showAlert(title: "", message: "Invalid link")
+            return
+        }
+        activityIndicator.show()
+        
+        InstaManager.getMediaPost(with: activeLink) { [unowned self] post in
+            activityIndicator.hide()
+            guard let post = post else {
+                showAlert(title: "", message: "Can't get the post, try again later")
+                return
+            }
+            let vc: PreviewViewController = PreviewViewController.loadFromStoryboard()
+            vc.post = post
+            present(vc, animated: true, completion: nil)
+        }
+    }
+    
+    private func showAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        alert.view.tintColor = UIColor(named: "customBlack")
+        present(alert, animated: true)
+    }
+    
+    private func openPreview() {
+        if let link = UIPasteboard.general.string {
+            showPreview(link)
+        } else {
+            showAlert(title: "", message: "Copy the Instagram link at first")
+        }
+    }
+    
     //MARK: - @IBActions
 
     @IBAction func settingsButtonPressed(_ sender: Any) {
     }
     
     @IBAction func pasteButtonPressed(_ sender: Any) {
+        if NetworkManager.shared.isNetworkAvailable {
+            if interstitialAd.isReady && AdManager.isAdEnabled {
+                interstitialAd.present(fromRootViewController: self)
+            } else {
+                openPreview()
+            }
+        } else {
+            NetworkManager.showNetworkReachabilityAlert(vc: self)
+        }
     }
 }
 
@@ -70,6 +113,6 @@ class MainViewController: UIViewController {
 extension MainViewController: GADInterstitialDelegate {
     func interstitialDidDismissScreen(_ ad: GADInterstitial) {
         setupInterstitialAd()
-        
+        openPreview()
     }
 }
