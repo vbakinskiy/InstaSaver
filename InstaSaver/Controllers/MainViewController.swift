8cd4848a95bc1bb3096e6a811b329bb2186e3913
Vyacheslav Bakinskiy <vyacheslav.bakinskiy@gmail.com>
2021-01-18 12:19:07 +0300

    Renamed the ViewController to MainViewController + Added the Paste button and Settings button

---
+++ InstaSaver/Controllers/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  InstaSaver
+//
+//  Created by Vyacheslav Bakinskiy on 1/18/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    
+    //MARK: - @IBOutlets
+    
+    @IBOutlet var pasteButton: UIButton!
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - @IBActions
+
+    @IBAction func settingsButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pasteButtonPressed(_ sender: Any) {
+    }
+    
+}
+
